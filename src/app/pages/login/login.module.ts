efce885a4790dc791721ba9f98471c3ed611c6e3
Giovanni <gperri39@gmail.com>
2022-06-03 17:58:56 +0200

    Gestione login tramite reactiveForm e disattivazione bottone in caso di errata immissione dei campi

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +15,8 @@ import { ToolbarInferioreModule } from 'src/app/components/toolbar-inferiore/too
     FormsModule,
     IonicModule,
     LoginPageRoutingModule,
-    ToolbarInferioreModule
+    ToolbarInferioreModule,
+    ReactiveFormsModule,
   ],
   declarations: [LoginPage]
 })
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginform;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) {
+    this.loginform = fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+   }
 
   ngOnInit() {
   }
